a9b05f2904a3d0861d121968cc05fed0de96a726
qqwangxiaow <ywwangxiaow@qq.com>
2020-02-25 13:38:08 +0800

    quicksort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,3 +1,44 @@
+//cpp version
+#include<iostream>
+#include<vector>
+using namespace std;
+int partition(vector<int>&arr,int l,int r)
+{
+    int pivot=arr[r];
+    int ll=l-1;
+    for(int i=l;i<r;i++)
+    {
+        if(arr[i]<=pivot)
+        {
+            ll++;
+            swap(arr[ll],arr[i]);
+        }
+    }
+    swap(arr[++ll],arr[r]); //arr[r] not pivot
+    return ll;
+}
+void quicksort(vector<int>&arr,int l,int r)
+{
+    if(l<r)  //if(l<r) not while
+    {
+        int m=partition(arr,l,r);
+        quicksort(arr,l,m-1);
+        quicksort(arr,m+1,r);
+    }
+
+}
+int main()
+{
+    vector<int>arr{2,1,9,-1,6,0,22};
+    quicksort(arr,0,arr.size()-1);
+    for(int i=0;i<arr.size();i++)
+    {
+        cout<<" "<<arr[i];
+    }
+    return 0;
+
+}
+
 //using first element as pivot
 void QuickSort(int A[],int left,int right)
 {
