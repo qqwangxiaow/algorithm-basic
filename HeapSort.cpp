fdab7680d70e1af81853a0ed0df3d2cb02f74dd4
wangxiaowei <wangxiaowei@bilibili.com>
2026-05-14 15:47:53 +0800

    add heap sort

--- HeapSort.cpp
+++ HeapSort.cpp
@@ -1,42 +1,44 @@
 #include<iostream>
 #include<vector>
-using namespace std;
-void adjust(vector<int>&arr,int len,int index)
-{
-    int left=2*index+1;
-    int right=2*index+2;
-    int maxindex=index;
-    if(left<len&&arr[left]>arr[maxindex])  //maxindex not index 
-        maxindex=left;
-    if(right<len&&arr[right]>arr[maxindex])
-        maxindex=right;
-    if(maxindex!=index)
-    {
-        swap(arr[maxindex],arr[index]);  
-        adjust(arr,len,maxindex); //the 3rd variable=maxindex
-    } 
+void adjust(std::vector<int>& nums, int i, int size) {
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    int max_index = i;
+    if (left < size && nums[max_index] < nums[left]) {
+        max_index = left;
+    }
+    if (right < size && nums[max_index] < nums[right]) {
+        max_index = right;
+    }
+    if (max_index != i) {
+        std::swap(nums[i], nums[max_index]);
+        adjust(nums, max_index, size);
+    }
+    return;
+
 }
 
-void heapsort(vector<int>&arr,int size)
-{
-    for(int i=size/2-1;i>=0;i--) //i=size/2-1
-    {
-        adjust(arr,size,i);
+void heapSort(std::vector<int>& nums) {
+    int n = nums.size();
+    // 要-1
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        adjust(nums, i, n);
     }
-    for(int i=size-1;i>=1;i--)  //i>=1
-    {
-        swap(arr[i],arr[0]);
-        adjust(arr,i,0);
+    for (int i = n - 1 ; i > 0; --i) {
+        std::swap(nums[i], nums[0]);
+        // 从 0 开始 size 是 i
+        adjust(nums, 0, i);
     }
 }
 
 int main()
 {
-    vector<int>arr{2,1,9,-1,6,0,22};
-    heapsort(arr,arr.size());
-    for(int i=0;i<arr.size();i++)
-    {
-        cout<<" "<<arr[i];
+    std::vector<int> arr{2,1,9,-1,6,0,22};
+    heapSort(arr);
+    for(int i = 0; i < arr.size(); i++) {
+        std::cout << " " << arr[i];
     }
 
 }
+
+
